011b823a95155e3dfa69caf450ecc9001bb8e132
Maarja <maarja-liis.kaldvee@voco.ee>
2023-01-18 16:40:57 +0200

    Ex 3 - ok

--- resistorrun.ts
+++ resistorrun.ts
@@ -1,9 +1,9 @@
 class Resistor {
     r: number = 0;
-    allowedPower: number = 0;
+    allowedPower: number = 0
     constructor(r: number, ap: number) {
         this.r = r;
-        this.allowedPower = ap;
+        this.allowedPower = ap
     }
     getCurrent(u: number): number {
         return u / this.r;
@@ -15,21 +15,19 @@ class Resistor {
 
 let r1 = new Resistor(220, 0.2);
 let r2 = new Resistor(110, 0.1);
-let r3 = new Resistor(1017, 5);
+let r3 = new Resistor(4700, 5);
 
 let resistors: Resistor[] = []
 resistors = [r1, r2, r3]
 
-function controlResistorAllowed(Resistros: Resistor[], u:number): Resistor[]{
+function controlResistorAllowed(resistors: Resistor[], u: number): Resistor[] {
     let allowedResistors: Resistor[] = []
-    resistors.forEach(resistors) => {
-        if(resistors.getPower(u) <= resistors.allowedPower){
-            allowedResistros.push(resistor)
+    resistors.forEach((resistor) => {
+        if(resistor.getPower(u) <= resistor.allowedPower){
+            allowedResistors.push(resistor);
         }
     })
-    return allowedResistros
+    return allowedResistors
 }
 
-if(r1.getPower(5) <= r1.allowedPower){
-    console.log(r1.getPower(5));
-}
+console.log(controlResistorAllowed(resistors, 5))
