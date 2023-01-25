d3257f2315713e6126b7d6d6e92bb956e37d7c36
Maarja <maarja-liis.kaldvee@voco.ee>
2023-01-25 14:30:42 +0200

    Ex 1 - test

--- resistorrun.ts
+++ resistorrun.ts
@@ -1,48 +1,37 @@
-class Resistor {
-    r: number = 0;
-    name: String;
-    constructor(r: number, name: String) {
-        this.r = r;
-        this.name = name;
+abstract class AbstractResistor {
+    abstract getResistance(): number;
+    getCurrent(u: number): number{
+        return u/this.getResistance();
+    }
+}
+
+class Switch extends AbstractResistor {
+    on: boolean = false;
+    setOn(state: boolean){
+        this.on=state;
     }
     getResistance(): number {
-        return this.r;
+        return (this.on ? 0 : 1000000000);
     }
-    getCurrent(u: number): number {
-        return u / this.getResistance();
-    }
-    getDescription() {
-        return this.name + " " + this.getResistance() + "ohm";
-    }
-}
-
-
-class SeriesCircuit extends Resistor {
-    resistors: Resistor[] = []
-    constructor(name: String) {
-        super(0, name);
-    }
-    push(r: Resistor) {
-        this.resistors.push(r);
-    }
-    getResistance() {
-        let sum: number = 0;
-        this.resistors.forEach((r: Resistor) => { sum += r.getResistance() });
-        return sum;
-    }
-
-    getDescription(): string {
-        return this.name + " " + this.getResistance() +  "ohm(" + this.resistors.map(r => r.getDescription()).join(", ") + ")";
+    getCurrent(u: number): number{
+        if(u>0){
+            if(this.on){
+                throw new Error("Short circuit");
+            }
+        }
+        return super.getCurrent(u);
     }
 }
 
-let r1: Resistor = new Resistor(10, "takisti1");
-let r2: Resistor = new Resistor(5, "takisti2");
-let r3: Resistor = new Resistor(10, "takisti3");
-let sc1: SeriesCircuit = new SeriesCircuit("jada1");
-let sc2: SeriesCircuit = new SeriesCircuit("jada2");
-sc2.push(r1);
-sc2.push(r2);
-sc1.push(sc2);
-sc1.push(r3);
-console.log(sc1.getDescription());
+function printResistance(r: AbstractResistor){
+    console.log(r.getResistance());
+}
+
+let r1: Switch = new Switch();
+r1.setOn(true);
+printResistance(r1);
+r1.setOn(false);
+printResistance(r1);
+console.log(r1.getCurrent(5));
+r1.setOn(true);
+console.log(r1.getCurrent(5));
